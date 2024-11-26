e3b869f98a173f2045782e9bb57f64b1eec616cf
sudha-priya-coder <sudhapriyaofficial@gmail.com>
2024-11-26 22:26:18 +0530

    first commit

--- client/src/apicalls/index.js
+++ client/src/apicalls/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    // baseURL: 'http://localhost:5000',
+    baseURL: 'https://mern-deploy-server-wyq0.onrender.com',
     headers: {
         Authorization: `bearer ${localStorage.getItem('token')}`
     }
--- server/server.js
+++ server/server.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const cors = require('cors');
+
+
 const app = express();
 require('dotenv').config();
+app.use(cors({
+    origin: 'http://localhost:3000',  // Allow requests from your local frontend
+    methods: 'GET,POST,PUT,DELETE',  // Allow specific HTTP methods
+    credentials: true  // If you're using cookies or authentication
+  }));
 app.use(express.json());
 const dbconfig = require('./config/dbconfig');
 
